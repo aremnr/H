da3644068e36d2e75e8389262b37d0e28850acb4
Ar-1a <armennen941@gmail.com>
2023-12-23 22:12:26 +0300

    2

--- pdf_file_anonymization/img_work.py
+++ pdf_file_anonymization/img_work.py
@@ -1,30 +1,13 @@
 import pytesseract
 from pytesseract import Output
 import cv2
-import pathlib
-import os
+
 
 def main():
     
-    img = cv2.imread(f"/home/ar/Documents/Hachaton/temp_imgs/out0.png")
+    img = cv2.imread(f"/home/ar/Documents/Hachaton/temp_imgs/out1.png")
     d = pytesseract.image_to_data(img, output_type=Output.DICT, lang="rus")
-    n_boxes = len(d['level'])
-    while(d["text"].count("")>0 or d["text"].count(" ")>0 or d["text"].count("  ")>0):
-        if d["text"].count("")>0:
-            i = d["text"].index("")
-        elif d["text"].count(" ")>0:
-            i = d["text"].index(" ")
-        elif d["text"].count("  ")>0:
-            i = d["text"].index("  ")
-        elif d["text"].count("-")>0:
-            i = d["text"].index("-")
-        d["level"].pop(i)
-        d['left'].pop(i)
-        d['top'].pop(i)
-        d['width'].pop(i)
-        d['height'].pop(i)
-        d["text"].pop(i)
-        
+
     n_boxes = len(d['level'])
     flag = False
     for i in range(n_boxes-1):
@@ -34,12 +17,8 @@ def main():
         elif flag:
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), -1)
             flag = False
-    print(d["text"])
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
-    exit()
+    cv2.imwrite('img.png', img)
 
-        
 
 
 
--- pdf_file_anonymization/pdf_to_img.py
+++ pdf_file_anonymization/pdf_to_img.py
@@ -1,9 +1,24 @@
-from pdf2image import convert_from_path
-import shutil
-import os
-shutil.rmtree("/home/ar/Documents/Hachaton/temp_imgs/")
-os.mkdir("/home/ar/Documents/Hachaton/temp_imgs/")
-def PdfToImg(pdf_path):
-    pages = convert_from_path("/home/ar/Documents/Hachaton/PDF/Example.pdf", dpi=800, size=3000)
-    for count, page in enumerate(pages):
-        page.save(f'/home/ar/Documents/Hachaton/temp_imgs/out{count}.png', 'PNG')
+# from pdf2image import convert_from_path
+# import shutil
+# import os
+# shutil.rmtree("/home/ar/Documents/Hachaton/temp_imgs/")
+# os.mkdir("/home/ar/Documents/Hachaton/temp_imgs/")
+# def PdfToImg(pdf_path):
+#     pages = convert_from_path("/home/ar/Documents/Hachaton/PDF/Example.pdf", dpi=800, size=3000)
+#     for count, page in enumerate(pages):
+#         page.save(f'/home/ar/Documents/Hachaton/temp_imgs/out{count}.png', 'PNG')
+
+
+import pypdfium2 as pdfium
+pdf = pdfium.PdfDocument("/home/ar/Documents/Hachaton/r.pdf")
+n_pages = len(pdf)
+for page_number in range(n_pages):
+    page = pdf.get_page(page_number)
+    pil_image = page.render(
+    scale=10,
+    rotation=0,
+    crop=(0, 0, 0, 0),
+
+    
+    ).to_pil()
+    pil_image.save(f"/home/ar/Documents/Hachaton/temp_imgs/out{page_number+1}.png")
